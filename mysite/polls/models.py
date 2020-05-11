e0a1e6ec5cef8c07012c2be94164c41fa39fc52a
Hanabi Ono <hanabi.ob@gmail.com>
2020-05-11 16:58:17 +0200

    Added Course and Review model

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+#from django.contrib.auth.models import User
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -17,5 +18,21 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=200)
+    catalogue_nr = models.CharField(max_length=10)
+    def __str__(self):
+        return self.course_name
+
+class Review(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    #user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=500)
+    rating = models.FloatField(default=0)
+    def __str__(self):
+        return self.course
+        #return self.user.username
+
     
 # Create your models here.
