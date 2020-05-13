68542af51ff9651831f205c144afbe45dfa53c55
Eva Gmelich Meijling <evameijling@gmail.com>
2020-05-13 17:36:28 +0200

    created user registration function

--- mysite/accounts/urls.py
+++ mysite/accounts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    ]
+    path("register/", views.register, name="register")
+]
--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .forms import *
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+# Register
+def register(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST or None)
+        # check if the form is valid
+        if form.is_valid():
+            user = form.save()
+
+            # get the password
+            raw_password = form.cleaned_data('password1')
+
+            #authenticate the user
+            user = authenticate(username-user.username, password-raw_password)
+
+            #login the user
+            login(request, user)
+
+            return redirect("main:home")
+
+        else:
+            form = RegistrationForm()
+        return render(request, "accounts/register.html", {"form": form})
+            
