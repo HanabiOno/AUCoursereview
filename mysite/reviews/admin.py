7f5ec3753a872a3299779af117abf0049e3dd79e
Hanabi Ono <hanabi.ob@gmail.com>
2020-05-13 17:44:20 +0200

    Added models Question and Choice in reviews app

--- mysite/reviews/admin.py
+++ mysite/reviews/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Course, Review
+from .models import Course, Review, Question, Choice
 
 admin.site.register(Course)
 admin.site.register(Review) 
+admin.site.register(Question)
+admin.site.register(Choice)
 
 # Register your models here.
--- mysite/reviews/models.py
+++ mysite/reviews/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class Course(models.Model):
     course_name = models.CharField(max_length=200)
@@ -15,4 +16,19 @@ class Review(models.Model):
         return self.comment
         #return self.user.username
 
+class Question(models.Model):  
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
+        
 # Create your models here.
