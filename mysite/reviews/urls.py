211841bc0eeb56219ab971ca958cb08776e98401
Hanabi Ono <hanabi.ob@gmail.com>
2020-05-13 16:10:31 +0200

    worked on views for reviews app

--- mysite/reviews/urls.py
+++ mysite/reviews/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'reviews'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:course_id>/', views.detail, name='detail'),
+    path('<int:course_id>/results/', views.results, name='results'),
+    path('<int:course_id>/review/', views.review, name='review'),
 ]
--- mysite/reviews/views.py
+++ mysite/reviews/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-
+from .models import Course
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the review page.")
+    latest_course_list = Course.objects.order_by('course_name')
+    context = {'latest_course_list': latest_course_list}
+    return render(request, 'reviews/index.html', context)
+
+def detail(request, course_id):
+    course = get_object_or_404(Course, pk=course_id)
+    return render(request, 'reviews/detail.html', {'course': course})
+
+def results(request, course_id):
+    reviews = "You're looking at the reviews of course: %s."
+    return HttpResponse(response % course_id)
+
+def review(request, course_id):
+    return HttpResponse("You're reviewing the course %s." % course_id)
 
 # Create your views here.
