12aaceff229a392171c0aa09cc9e4182ad40e6ae
Hanabi Ono <hanabi.ob@gmail.com>
2020-05-13 18:39:16 +0200

    Results are already shown on coursepage (detail.html)

--- mysite/reviews/urls.py
+++ mysite/reviews/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'reviews'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_iew(), name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     #results page is not necessary if we want to show the reviews on the index page of the course already
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    #path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:course_id>/review/', views.review, name='review'),
 ]
