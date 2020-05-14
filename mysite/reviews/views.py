ce5dfc2151735d5d3f5f307d94a93e74c4b2879a
jonathan_wheeler <j.a.whl@icloud.com>
2020-05-14 09:32:29 +0000

    started html for reviews.html

--- mysite/reviews/views.py
+++ mysite/reviews/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Course, Review
 from django.template import loader
